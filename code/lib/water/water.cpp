b1b57eb57a4c8f185f9cfc1b87b1951e05659eb3
NitroGriffin <36413096+NitroGriffin@users.noreply.github.com>
2018-05-07 23:17:17 +0300

    Update water.cpp

--- code/lib/water/water.cpp
+++ code/lib/water/water.cpp
@@ -1,1 +1,18 @@
 #include <water.h>
+#include <fire.h>
+#include <Arduino.h>
+
+#define WPM 7
+
+//@todo
+//make circuit for pump
+
+void Water_setup(void) {
+  pinMode(WPM, OUTPUT);
+}
+
+void Water_activate(void) {
+  while(Fire_detect()) {
+    digitalWrite(WPM, HIGH);
+  }
+}
