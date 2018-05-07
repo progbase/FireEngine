dc4d50adeb0d0baad8736be5fa4810d47f2c03a4
NitroGriffin <36413096+NitroGriffin@users.noreply.github.com>
2018-05-07 23:14:07 +0300

    Update main.cpp

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,1 +1,21 @@
+#include <wheels.h>
+#include <fire.h>
+#include <water.h>
+#include <Arduino.h>
 
+void setup() {
+  Fire_setup();
+  Wheels_setup();
+}
+
+void loop() {
+  if(Fire_detect()) {
+    Wheels_stop();
+    while(Fire_detect() && !Fire_isHot()) {
+      Wheels_forward();
+    }
+    Wheels_stop();
+    if(Fire_detect()) Water_activate();
+  }
+  Wheels_right();
+}
