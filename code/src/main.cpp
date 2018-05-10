4ccf5cff2cb07fd7b4148c31b41d90ff27028386
vsmetansky <36275660+vsmetansky@users.noreply.github.com>
2018-05-11 01:33:12 +0300

    Update main.cpp

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,21 +1,29 @@
 #include <wheels.h>
 #include <fire.h>
 #include <water.h>
+#include <obstacles.h>
 #include <Arduino.h>
 
 void setup() {
   Fire_setup();
   Wheels_setup();
+  Obstacles_setup();
+  Serial.begin(9600);
 }
 
 void loop() {
   if(Fire_detect()) {
     Wheels_stop();
-    while(Fire_detect() && !Fire_isHot()) {
+    while(!Obstacles_detect() && !Fire_isHot()) {
       Wheels_forward();
+      //if(!Fire_detect()) break;
     }
     Wheels_stop();
-    if(Fire_detect()) Water_activate();
+    if(Obstacles_detect()) {
+      Obstacles_avoid();
+    } else if(Fire_isHot()) {
+      Water_activate();
+    }
   }
-  Wheels_right();
+  Wheels_left();
 }
