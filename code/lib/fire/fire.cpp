52fa4ae06b4bd5a306285acde429908e4c1aa671
vsmetansky <36275660+vsmetansky@users.noreply.github.com>
2018-05-11 01:34:38 +0300

    Update fire.cpp

--- code/lib/fire/fire.cpp
+++ code/lib/fire/fire.cpp
@@ -2,16 +2,20 @@
 #include <stdbool.h>
 #include <Arduino.h>
 
-#define FSR 6
+#define FSR_DIGITAL 6
+#define FSR_ANALOG A0
+//max value given by sensor when the fire source is too close
+#define CRITICAL_VAL 25
 
 static void wait(void);
 
 void Fire_setup(void) {
-  pinMode(FSR, INPUT);
+  pinMode(FSR_DIGITAL, INPUT);
+  pinMode(FSR_ANALOG, INPUT);
 }
 
 bool Fire_detect(void) {
-  if (digitalRead(FSR) == 0) {
+  if(digitalRead(FSR_DIGITAL) == 1) {
     wait();
     return true;
   }
@@ -19,9 +23,13 @@ bool Fire_detect(void) {
 }
 
 bool Fire_isHot(void) {
+  if(analogRead(FSR_ANALOG) <= CRITICAL_VAL) {
+    return true;
+  }
   return false;
 }
 
 static void wait(void) {
   delay(300);
 }
+
